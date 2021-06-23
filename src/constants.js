62d0f4d3aae2eb80132b7069163a8136a7aa09c7
36changes <85654444+36changes@users.noreply.github.com>
2021-06-24 06:32:39 +1000

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,60 +1,20 @@
 import * as Web3 from 'web3'
-import BigNumber from 'bignumber.js'
-import { PortisProvider } from 'portis'
+import { OpenSeaPort, Network } from 'opensea-js'
 
-export const GOOGLE_ANALYTICS_ID = 'UA-111688253-4'
-export const OPENSEA_URL = "https://opensea.io"
-export const OPENSEA_JS_URL = "https://github.com/ProjectOpenSea/opensea-js"
-export const GITHUB_URL = "https://github.com/ProjectOpenSea/ships-log"
-export const DEFAULT_DECIMALS = 18
-export let web3Provider = typeof web3 !== 'undefined'
-  ? window.web3.currentProvider
-  : new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/03672b06fd384bc4bd05af5e65c343e0')
+// This example provider won't let you make transactions, only read-only calls:
+const provider = new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/03672b06fd384bc4bd05af5e65c343e0')
 
-// Replace this with Redux for more complex logic
-const networkCallbacks = []
-export const onNetworkUpdate = (callback) => {
-  networkCallbacks.push(callback)
-}
+const seaport = new OpenSeaPort(provider, {
+  networkName: Network.Main
+})
 
-export async function connectWallet() {
-  if (!window.web3) {
-    web3Provider = new PortisProvider({
-      // Put your Portis API key here
-    })
-  } else if (window.ethereum) {
-    window.ethereum.enable()
-  } else {
-    const errorMessage = 'You need an Ethereum wallet to interact with this marketplace. Unlock your wallet, get MetaMask.io or Portis on desktop, or get Trust Wallet or Coinbase Wallet on mobile.'
-    alert(errorMessage)
-    throw new Error(errorMessage)
-  }
-  networkCallbacks.map((c) => c(web3Provider))
-}
+// Address allowed to buy from you
+const buyerAddress = "0x4a861fdbfff083b4c87be25ebba96c3751336e1d"
 
-export function toUnitAmount(baseAmount, tokenContract = null) {
-  const decimals = tokenContract && tokenContract.decimals != null
-    ? tokenContract.decimals
-    : DEFAULT_DECIMALS
-
-  const amountBN = new BigNumber(baseAmount.toString())
-  return amountBN.div(new BigNumber(10).pow(decimals))
-}
-
-export function toBaseUnitAmount(unitAmount, tokenContract = null) {
-  const decimals = tokenContract && tokenContract.decimals != null
-    ? tokenContract.decimals
-    : DEFAULT_DECIMALS
-
-  const amountBN = new BigNumber(unitAmount.toString())
-  return amountBN.times(new BigNumber(10).pow(decimals))
-}
-
-export async function promisify(inner) {
-  return new Promise((resolve, reject) =>
-    inner((err, res) => {
-      if (err) { reject(err) }
-      resolve(res)
-    })
-  )
-}
+const listing = await seaport.createSellOrder({
+  tokenAddress: "0x0b08af3b5a4877ae471342342743f7cb86db604b", // Decentraland
+  tokenId: "231", // Token ID
+  accountAddress: "0x2854d97fBfC296f53118270601f193aa77cf06da",
+  startAmount: 0,
+  buyerAddress
+})
